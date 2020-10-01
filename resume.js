e8d3d5053b28552d1664331fad0191a5d9bfc4bf
Gregory VALIGIANI <gregory.valigiani@amadeus.com>
2020-10-01 18:02:15 +0200

    [resume] html + css + js

---
+++ resume.js
@@ -0,0 +1,315 @@
+
+// //////////////////////////////////////////////////
+// resume-data
+
+var resume_data = {
+    "firstName": "Gregory",
+    "lastName": "Valigiani",
+    "title": [
+      "Senior Software Engineer",
+      "+12 years experience",
+      "Ph. D. Computer Science"
+    ],
+    "experience": [{
+        "title": "Senior Software Engineer",
+        "duration": {
+            "start": 2008,
+            "end": "Present"
+        },
+        "locations": [{
+            "name": "Amadeus Company",
+            "sub-name": "Hotel Department",
+            "city": "Sophia Antipolis",
+            "country": "FR"
+        }],
+        "achievments": [{
+            "descriptions": ["Design and Implement streaming process to synchronize Hotel shopping data with external partners"],
+            "stack": {
+                "os": ["Windows"],
+                "code": ["Java"],
+                "techno": ["Spring","Mockito","Kafka","Quartz","Protobuf"],
+                "base": ["Couchbase"],
+                "ide": ["Intellij"],
+                "version": ["Git"],
+                "script": ["Shell"]
+            }
+        },
+        {
+            "descriptions": ["Implement responsive and ultra-high available back-end functionalities on Hotel availability engine"],
+            "stack": {
+                "os": ["Windows", "Linux"],
+                "code": ["C++"],
+                "base": ["Couchbase"],
+                "techno": ["Protobuf"],
+                "ide": ["Eclipse"],
+                "version": ["Git", "Mercurial"],
+                "script": ["Python"]
+            }
+        },
+        {
+            "descriptions": ["Implement administrative back-end functionalities on Hotel platform"],
+            "stack": {
+                "os": ["Windows", "Linux"],
+                "code": ["C++"],
+                "base": ["Oracle"],
+                "ide": ["Eclipse"],
+                "version": ["Cvs"],
+                "script": ["Python"]
+            }
+        }]
+    },
+    {
+        "title": "Junior Software Consultant",
+        "duration": {
+            "start": 2006,
+            "end": 2008
+        },
+        "locations": [{
+            "name": "Coframi Company",
+            "city": "Sophia Antipolis",
+            "country": "FR"
+        }],
+        "achievments": [{
+            "descriptions": ["Consultant for Amadeus Company, Hotel Department", "Design and improve user interface on Hotel platform"],
+            "stack": {
+                "os": ["Windows"],
+                "code": ["Java","Javascript"],
+                "techno": ["Struts","Jsp","Webwork"],
+                "ide": ["Eclipse"],
+                "version": ["Cvs"]
+            }
+        },
+        {
+            "descriptions": ["Consultant for Amadeus Company, Quality Department", "Implement data visualization application for model based testing"],
+            "stack": {
+                "os": ["Windows"],
+                "code": ["Java"],
+                "base": ["MsSQL"],
+                "techno": ["Swing"],
+                "ide": ["Eclipse"],
+                "version": ["ClearCase"]
+            }
+        }]
+    },
+    {
+        "title": "Ph.D. Candidate, Computer science",
+        "duration": {
+            "start": 2003,
+            "end": 2006
+        },
+        "locations": [{
+            "name": "Paraschool Company",
+            "city": "Paris",
+            "country": "FR"
+        },
+        {
+            "name": "INRIA Institute",
+            "city": "Rocquencourt",
+            "country": "FR"
+        },
+        {
+            "name": "Calais University",
+            "city": "Calais",
+            "country": "FR"
+        }],
+        "achievments": [{
+            "descriptions": ["Set up automatic process to adapt proposal of e-learning exercises", "Increase user interaction with adaptive avatar technology"],
+            "stack": {
+                "os": ["Windows"],
+                "code": ["Java"],
+                "techno": ["Swing","Jsp","Struts"],
+                "ide": ["Eclipse"],
+                "technic": ["Ant Colony Optimisation"]
+            }
+        }]
+    },
+    {
+        "title": "Research Intern",
+        "duration": {
+            "year": 2003,
+            "duration": "3 months"
+        },
+        "locations": [{
+            "name": "Calais University",
+            "city": "Calais",
+            "country": "FR"
+        }],
+        "achievments": [{
+            "descriptions": ["Remove atmospheric disruptions on satellite photos to detect phytoplancton"],
+            "stack": {
+                "os": ["Linux"],
+                "code": ["C"],
+                "ide": ["Vim"],
+                "technic": ["Artificial Evolution"]
+            }
+        }]
+    },
+    {
+        "title": "Research Intern",
+        "duration": {
+            "year": 2002,
+            "duration": "2 months"
+        },
+        "locations": [{
+            "name": "Napier University",
+            "city": "Edimburgh",
+            "country": "GB"
+        }],
+        "achievments": [{
+            "descriptions": ["Solve timetables problem using artificial evolution"],
+            "stack": {
+                "os": ["Linux"],
+                "code": ["C", "Java"],
+                "ide": ["Vim"],
+                "technic": ["Artificial Evolution"]
+            }
+        }]
+    }],
+    "education": [{
+        "title": "PhD Thesis",
+        "duration": {
+            "start": 2003,
+            "end": 2006
+        },
+        "locations": [{
+            "name": "Calais University",
+            "city": "Calais",
+            "country": "FR"
+        }],
+        "subjects": ["Optimization", "Artificial Evolution", "e-Learning"]
+    },
+    {
+        "title": "Research Master",
+        "duration": {
+            "start": 2002,
+            "end": 2003
+        },
+        "locations": [{
+            "name": "Paris VI University",
+            "city": "Paris",
+            "country": "FR"
+        }],
+        "subjects": ["Artificial Evolution", "Pattern Recognition"]
+    },
+    {
+        "title": "Engineering Master",
+        "duration": {
+            "start": 2000,
+            "end": 2003
+        },
+        "locations": [{
+            "name": "ENSTA Engineering School",
+            "city": "Paris",
+            "country": "FR"
+        }],
+        "subjects": ["Computer vision", "Robotic"]
+    }],
+    "publication": [{
+        "title": "Étude Comportementale des Hommilières pour l'Optimisation",
+        "authors": ["Grégory Valigiani", "Évelyne Lutton", "Pierre Collet"],
+        "in": "EpiNet, EPI Electronic Magazine, n° 83, march 2006, Web Article"
+    },
+    {
+        "title": "Automatic Rating Process to Audit a Man-Hill",
+        "authors": ["Grégory Valigiani", "Évelyne Lutton", "Yannick Jamont", "Raphaël Biojout", "Pierre Collet"],
+        "in": "In WSEAS Transactions on Advances in Engineering Education, p1-7, Issue 1, Volume 3, January, ISSN 1790-1979, 2006, Refereed Journal Article"
+    },
+    {
+        "title": "Evaluating a Real-Size Man-Hill",
+        "authors": ["Grégory Valigiani", "Évelyne Lutton", "Yannick Jamont", "Raphaël Biojout", "Pierre Collet"],
+        "in": "ECE'WSEAS'05E, 17-19 November, Miami, Florida, USA, 2005, Refereed Conference Contribution"
+    },
+    {
+        "title": "Experimenting with a Real-Size Man-Hill to Optimize Pedagogical Paths",
+        "authors": ["Grégory Valigiani", "Raphaël Biojout", "Yannick Jamont", "Évelyne Lutton", "Pierre Collet"],
+        "in": "SAC'05 Proceedings, 13-17 mars, Santa Fe, New Mexico, USA, 2005, Refereed Conference Contribution"
+    },
+    {
+        "title": "Analysis of GP Improvement Techniques over the Real-World Inverse Problem of Ocean Colour",
+        "authors": ["Grégory Valigiani", "Cyril Fonlupt", "Pierre Collet"],
+        "in": "Genetic Programming 7th European Conference, EuroGP 2004, Proceedings, LNCS, Vol. 3003, pp. 174-186, Springer-Verlag, 5-7 April 2004, Refereed Conference Contribution"
+    }],
+    "contact": {
+      "phone": "+33.6.20.83.65.54",
+      "email": "gregory.valigiani@gmail.com",
+      "address": "Cagnes sur mer, FR",
+      "linkedin": "linkedin.com/in/gregoryvaligiani"
+    },
+    "skill-types": [
+      {
+        "type": "code",
+        "skills": [
+          {
+            "name": "C++",
+            "level": 4
+          },{
+            "name": "Java",
+            "level": 4
+          }
+        ]
+      },{
+        "type": "script",
+        "skills": [
+          {
+            "name": "Python",
+            "level": 4
+          },{
+            "name": "Shell",
+            "level": 4
+          }
+        ]
+      },{
+        "type": "base",
+        "skills": [
+          {
+            "name": "Couchase",
+            "level": 4
+          },{
+            "name": "Oracle",
+            "level": 4
+          }
+        ]
+      },{
+        "type": "version",
+        "skills": [
+          {
+            "name": "Git",
+            "level": 4
+          },{
+            "name": "Cvs",
+            "level": 3
+          },{
+            "name": "Mercurial",
+            "level": 2
+          }
+        ]
+      },{
+        "type": "techno",
+        "skills": [
+          {
+            "name": "Spring",
+            "level": 3
+          },{
+            "name": "Mockito",
+            "level": 4
+          },{
+            "name": "Kafka",
+            "level": 4
+          },{
+            "name": "Quartz",
+            "level": 3
+          },{
+            "name": "Protobuf",
+            "level": 4
+          }
+        ]
+      }
+    ],
+    "languages": [{
+      "name": "French",
+      "level": "Native"
+    },{
+      "name": "English",
+      "level": "Business"
+    }]
+};
